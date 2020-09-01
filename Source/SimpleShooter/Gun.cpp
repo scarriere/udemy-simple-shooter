f5d793cdccea8dfd963ad8020ff96b6ede80f569
Samuel Carriere <carriere911@gmail.com>
2020-08-31 21:48:14 -0700

    Course 185: Impact Effects

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -4,6 +4,7 @@
 #include "Gun.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AGun::AGun()
@@ -21,6 +22,24 @@ AGun::AGun()
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if(OwnerPawn == nullptr) return;
+	AController* OwnerController = OwnerPawn->GetController();
+	if (OwnerController == nullptr) return;
+
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+	OwnerController->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+
+	FVector End = ViewPointLocation + ViewPointRotation.Vector() * MaxRange;
+
+	FHitResult Hit;
+	if (GetWorld()->LineTraceSingleByChannel(Hit, ViewPointLocation, End, ECollisionChannel::ECC_GameTraceChannel1))
+	{
+		FVector ShotDirection = -ViewPointRotation.Vector();
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+	}
 }
 
 // Called when the game starts or when spawned
